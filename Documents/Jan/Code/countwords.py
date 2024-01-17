b50f30e285e0a5e702faefb011c8bf45236ba797
Run Tao <Taoli_Run@outlook.com>
2024-01-17 22:30:52 +0800

    generated by AI

---
+++ Documents/Jan/Code/countwords.py
@@ -0,0 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import librosa
+
+data_path = 'Data/mandarin/audio/GeneratedByAI'
--- Documents/Jan/Code/readwords.py
+++ Documents/Jan/Code/readwords.py
@@ -1,21 +1,21 @@
 import pyttsx3
 import os
 
-with open('Data/order.txt','r',encoding='utf-8') as f:
+data_path = "D:\\Taoli\\Projects\\DialectTranslation\\Data\\"
+with open(data_path+'order.txt', 'r', encoding='utf-8') as f:
     words = f.readlines()
     for i in range(len(words)):
         words[i] = words[i].strip('\n')
 
 
-
-save_folder = "Data\\mandarin\\audio\\ReadbyAI"
-os.makedirs(save_folder, exist_ok=True)  # 创建存储音频文件的文件夹
+save_folder = "mandarin\\audio\\GeneratedByAI"
+os.makedirs(data_path+save_folder, exist_ok=True)  # 创建存储音频文件的文件夹
 
 engine = pyttsx3.init()
 
 for i, word in enumerate(words):
     filename = f"mandarin_{i}_AI.wav"
-    filepath = os.path.join(save_folder, filename)
+    filepath = os.path.join(data_path, save_folder, filename)
 
     engine.save_to_file(word, filepath)
     engine.runAndWait()
