8219cb7e13242976cd28aa85d4d6cbbb183093d4
Run Tao <Taoli_Run@outlook.com>
2024-01-18 22:05:33 +0800

    edited in Pycharm

--- Documents/Nov/Code/EnergyDistribution.py
+++ Documents/Nov/Code/EnergyDistribution.py
@@ -4,19 +4,26 @@ import librosa
 import librosa.display
 import matplotlib.pyplot as plt
 import math
+
+# matplotlib for chinese
 plt.rcParams['font.sans-serif'] = ['SimHei']
-def generate_energy_distribution(audio_file, label):
-    y, sr = librosa.load(audio_file)
-    energy = librosa.feature.rms(y=y)
-    return energy, label
+
+
+def generate_energy_distribution(filename, label_name):
+    y, sr = librosa.load(filename)
+    energy_feature = librosa.feature.rms(y=y)
+    return energy_feature, label_name
+
+
 energy_data = []
-with open('Data/order.txt','r',encoding='utf-8') as f:
+data_path = r"D:/TaoLi/Projects/DialectTranslation/Data"
+with open(data_path+'/order.txt', 'r', encoding='utf-8') as f:
     labels = f.readlines()
     for i in range(len(labels)):
         labels[i] = labels[i].strip('\n')
-for i in range(1, 23):
-    audio_file = f'Data/mandarin/audio/preprocessed/timelength/mandarin_{i}_scaled_1.5s.wav'
-    energy, label = generate_energy_distribution(audio_file, labels[i-1])
+for i in range(1, 100):
+    audio_file = data_path + f'/mandarin/audio/GeneratedByAI/mandarin_{i-1}_AI.wav'
+    energy, label = generate_energy_distribution(audio_file, labels[i - 1])
     energy_data.append((energy, label))
 num_images = len(energy_data)
 num_cols = 5
@@ -32,5 +39,5 @@ for i in range(num_images, num_rows * num_cols):
     fig.delaxes(axes.flatten()[i])
 plt.tight_layout(rect=[0, 0.01, 1, 0.95])
 plt.suptitle('能量分布')
-plt.savefig('Data/mandarin/energydistribution.png')
+plt.savefig(data_path+'/mandarin/energy_distribution_AI.png')
 plt.show()
