8fd8c32351b257bd8cf88be32acaf44f4d96c673
Run Tao <Taoli_Run@outlook.com>
2024-05-11 23:27:51 +0800

    20240511

--- Animation/DialectTranslation.py
+++ Animation/DialectTranslation.py
@@ -36,10 +36,10 @@ class DialectTranslation(Scene):
         ).to_edge(RIGHT)
 
         # 生成在[-1, 1]范围内的随机点
-        points = np.random.random((100, 2)) * 2 - 1
+        points = np.random.random((50, 2)) * 2 - 1
         # 创建散点并指定点的大小为0.04
         scatter = [Dot(radius=0.04, color=BLUE).move_to(left_axes.coords_to_point(x, y)) for x, y in points]
-        points = np.random.random((100, 2)) * 2 - 1
+        points = np.random.random((50, 2)) * 2 - 1
         # 将散点添加到左侧坐标轴上
         left_dots = VGroup(*scatter)
         self.add(left_axes, left_dots)
@@ -47,8 +47,11 @@ class DialectTranslation(Scene):
         # 为右侧坐标轴创建一个相同的点集
         right_scatter = [Dot(radius=0.04, color=YELLOW).move_to(right_axes.coords_to_point(x, y)) for x, y in points]
         right_dots = VGroup(*right_scatter)
-        self.add(right_axes, right_dots)
+        self.play(FadeIn(right_axes), FadeIn(right_dots))
 
+        Axe_name_left = Tex("\mathcal{M}, stands for \mathbf{M}andarin").next_to(left_axes, UP)
+        Axe_name_right = Tex("\mathcal{D}, stands for \mathbf{D}ialect").next_to(right_axes, UP)
+        self.play(Write(Axe_name_left), Write(Axe_name_right))
         self.wait(2)
 
         group = VGroup(left_axes, left_dots)
@@ -63,10 +66,36 @@ class DialectTranslation(Scene):
         group_2_copy = group_2.copy()
         origingroup_2 = group_2.copy()
         origingroup_2.move_to(ORIGIN)
-
+        group_blueyellow = VGroup(origingroup,origingroup_2)
+        group_blueyellow_copy = VGroup(origingroup, origingroup_2).scale(1.5)
         # 创建移动动画
-        self.play(Transform(group_copy,origingroup))
-        self.play(Transform(group_2_copy, origingroup_2))
+        self.play(FadeOut(Axe_name_left), FadeOut(Axe_name_right), FadeOut(group), FadeOut(group_2), Transform(group_copy,origingroup), Transform(group_2_copy, origingroup_2))
+        self.play(ReplacementTransform(group_blueyellow,group_blueyellow_copy))
+        self.wait(2)
+        # 若有空，做一个蓝色点平移到黄色点上的视频，以体现映射效果
+
+        self.play(*[FadeOut(mob) for mob in self.mobjects])
+
+        title = Text("Research Methods", font="Times New Roman",font_size=32).to_edge(UL)
+        self.play(Write(title))
+        self.wait(2)
+
+        method_SVD = Text("SVD", font_size=26, font="Times New Roman").move_to(ORIGIN)
+        method_MFCCs = Text("MFCCs", font="Times New Roman", font_size=26).next_to(method_SVD, UP, buff =0.5)
+        
+        method_Map = Text("Map", font_size=26, font="Times New Roman").next_to(method_SVD, DOWN, buff =0.5)
+
+        self.play(Write(method_MFCCs), Write(method_SVD), Write(method_Map))
+        self.wait(2)
+        method_SVD_copy = method_SVD.to_edge(LEFT)
+        self.play(Transform(method_SVD, method_SVD_copy), Transform(method_MFCCs, method_MFCCs.next_to(method_SVD, UP, buff=0.5)), Transform(method_Map, method_Map.next_to(method_SVD, DOWN, buff=0.5)))
+        self.wait(2)
+
+        MFCCS_full = Text("Full name: Multi-Frequency Cepstral Coefficients", font="Times New Roman", font_size=16).next_to(method_MFCCs, RIGHT)
+        SVD_full = Text("Full name: Singular Value Decomposition", font="Times New Roman", font_size=16).next_to(method_SVD, RIGHT)
+        MAP_explanation = Text("Using Cosine Distance and Linear Regression to find a map for dialects", font="Times New Roman", font_size=16).next_to(method_Map, RIGHT)
+        # 遗留问题 动画没做好
+        self.play(Write(MFCCS_full), Write(SVD_full), Write(MAP_explanation))
         self.wait(2)
 
 if __name__ == "__main__":
--- Animation/topsis.py
+++ Animation/topsis.py
@@ -15,8 +15,8 @@ class TOPSIS(Scene):
 
         self.play(FadeOut(topsis_full_name))
         self.play(FadeOut(title))
-        grid = NumberPlane(axis_config={"stroke_color": WHITE, "stroke_width": 0, "include_ticks": False})
-        self.add()
+        grid = NumberPlane(color=GREY)
+        self.add(grid)
         self.play(GrowFromCenter(grid))
         ''' axes = Axes(
             x_range=[0, 5, 1],  # x轴的范围从0到10，步长为1
@@ -43,6 +43,58 @@ class TOPSIS(Scene):
         Arrow_cos = Arrow(start=axes.coords_to_point(0, 0), end=axes.coords_to_point(2, 3.5), buff=0)
         self.play(GrowArrow(Arrow_cos))
         self.wait(1)'''
+
+        Dot_1 = Dot(radius=0.04, color=BLUE).move_to(grid.coords_to_point(2, 3.5))
+        Dot_2 = Dot(radius=0.04, color=BLUE).move_to(grid.coords_to_point(3, 2.7))
+
+        self.play(FadeIn(Dot_1), FadeIn(Dot_2))
+
+        Arrow_1 = Arrow(start=grid.coords_to_point(0, 0), end=grid.coords_to_point(2, 3.5), buff=0, color=RED)
+
+        Arrow_2 = Arrow(start=grid.coords_to_point(0,0), end=grid.coords_to_point(3,2.7), buff=0, color=YELLOW)
+
+        Distance = Line(grid.coords_to_point(2,3.5), grid.coords_to_point(3,2.7), color=GREEN)
+
+        self.play(GrowArrow(Arrow_1), GrowArrow(Arrow_2))
+        self.play(GrowArrow(Distance))
+        self.wait(1)
+
+        Equation_1 = Tex("D_2 = \sqrt{(x_1-x_2)^2+(y_1-y_2)^2}", font_size=28).to_corner(UL, buff=0.5)
+        
+        self.play(Transform(Distance,Equation_1))
+
+        self.play(Transform(Equation_1, Equation_1.shift(RIGHT*3)), FadeOut(Equation_1))
+        self.wait(1)
+
+        TwoDimension = Text("Two Dimension", font="Times New Roman", font_size=16).to_corner(UL, buff = 0.5)
+
+        self.play(Write(TwoDimension))
+        self.wait(1)
+        MultipleDimensions = Text("Multi-Dimensions", font="Times New Roman", font_size=16).next_to(TwoDimension, DOWN, buff = 0.5)
+
+        self.play(Write(MultipleDimensions))
+
+        '''Equation_1_copy = Equation_1.copy()
+        self.play(Transform(Distance,Equation_1))
+        self.wait(1)
+
+        TwoDimension = Text("Two Dimension", font="Times New Roman", font_size=16).to_corner(UL, buff = 0.5)'''
+
+        '''MultipleDimensions = Text("Multi-Dimensions", font="Times New Roman", font_size=16).next_to(TwoDimension, DOWN, buff = 0.5)
+
+        self.play(Transform(Equation_1_copy, Equation_1.copy().shift(RIGHT*3), rate_func=smooth), FadeOut(Equation_1))
+
+        self.play(Write(TwoDimension))
+
+        self.wait(1)
+        self.play(Write(MultipleDimensions))'''
+
+        DownArrow = Tex("\Downarrow").next_to(Equation_1, DOWN)
+
+        self.play(GrowFromPoint(DownArrow, Equation_1.get_center()))
+        Equation_2 = Tex("D_n = \sqrt{\sum_{i=1}^n(x_i-x_j)^2+(y_i-y_j)^2}", font_size=28).next_to(DownArrow, DOWN)
+        self.play()
+        self.wait(1)
 
 if __name__ == "__main__":
     from os import system
